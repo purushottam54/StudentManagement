3d4d52263f1cfdb30a950fd2bd58261eb622d6d9
om shingare <omshingare120@gmail.com>
2024-04-08 11:36:33 +0530

    Code Added

--- apply_room.php
+++ apply_room.php
@@ -38,7 +38,7 @@
 				<tbody>
 				<?php
                 $i = 1;
-				$isFull = ["Full","Not-Full"];
+				$isFull = ["Full"];
                 $qry = $conn->query("SELECT * FROM room order by room_id asc");
                 while($row = $qry->fetch_assoc()):                
                 ?>
@@ -53,20 +53,28 @@
 							<p><b><?php echo ucwords($row4['name']) ?></b></p>
 							<p class="truncate"></p>
 						</td>
-						<td><b><?php echo $isFull[$row['is_full']] ?></b></td>
+						<td><b><?php 
+						
+							if (($row['room_max'] - $row['room_current']) == 0) {
+								echo "Room Is FUll";
+							}
+							else{
+								echo $row['room_max'] - $row['room_current']." Remaining";
+							}
+						?></b></td>
 						<td><b><?php echo $row['room_price'] ?></b></td>
-						<td><b><?php echo $row['room_current'] ?></b></td>
+						<td><b><?php echo $row['room_max'] ?></b></td>
 						<td class="text-center">
 							<?php
-							  	echo $row['room_max'];
+							  	echo $row['room_current'];
 							?>
 						</td>
 						<td class="text-center">
 							<button type="button" class="btn btn-default btn-sm btn-flat border-info wave-effect text-info dropdown-toggle" data-toggle="dropdown" aria-expanded="true">
 		                      Action
 		                    </button>
-		                    <div class="dropdown-menu" style="">
-		                      <a class="dropdown-item view_project" href="./index.php?page=view_letter&id=<?php echo $row['room_id'] ?>" data-id="<?php echo $row['room_id'] ?>">View</a>
+							<div class="dropdown-menu" style="">
+		                      <a class="dropdown-item apply_room" id="check1" href="javascript:void(0)" data-id="<?php echo $row['room_id'] ?>">Apply</a>
 		                    </div>
 						</td>
 					</tr>	
@@ -85,26 +93,54 @@
 	}
 </style>
 <script>
+
+<?php
+	$qry1 = $conn->query("SELECT student_id FROM students WHERE stud_user_id = '".$_SESSION['login_user_id']."'");
+	$row5 = $qry1->fetch_assoc();
+?>
 	$(document).ready(function(){
 		$('#list').dataTable()
 	
-	$('.delete_project').click(function(){
-	_conf("Are you sure to delete this letter?","delete_project",[$(this).attr('data-id')])
+	$('.apply_room').click(function(){
+	_conf("Are you sure to apply this Room?","delete_project",[$(this).attr('data-id'), <?php echo $row5['student_id']?>])
 	})
 	})
-	function delete_project($id){
-		start_load()
+
+	
+	function delete_project($room_id, $student_id){
+		
+		start_load();
+		
+		
 		$.ajax({
-			url:'ajax.php?action=delete_project',
+			url:'ajax.php?action=apply_room',
 			method:'POST',
-			data:{id:$id},
+			data:{room_id:$room_id, student_id:$student_id},
 			success:function(resp){
+				console.log(resp);
 				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
+					alert_toast("Student Alerady In Room (ANY)",'danger')
 					setTimeout(function(){
 						location.reload()
 					},1500)
-
+				}
+				if(resp==0){
+					alert_toast("Student Successfully Applied For Room",'success')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+				}
+				if(resp==3){
+					alert_toast("Room is full!!",'danger')
+					setTimeout(function(){
+						location.reload()
+					},1500)
+				}
+				if(resp==2){
+					alert_toast("DB Error",'danger')
+					setTimeout(function(){
+						location.reload()
+					},1500)
 				}
 			}
 		})
--- home.php
+++ home.php
@@ -96,3 +96,149 @@
       </div>
 
   <?php endif;?>
+
+
+<?php if($_SESSION['login_user_type_id'] == 3): ?>
+<div class="row">
+    <div class="col-8">
+        <div class="card card-outline card-success">
+            <div class="card-header">
+                <b>Students In Room</b>
+            </div>
+            <div class="card-body p-0">
+                <div class="table-responsive">
+                    <table class="table m-0 table-hover">
+                        <colgroup>
+                            <col width="10%">
+                            <col width="30%">
+                            <col width="30%">
+                            <col width="30%">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Room ID</th>
+                                <th>Student Name</th>
+                                <th>Student Surname</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+
+                            $where = "WHERE stud_room_id IN ( SELECT room_id  FROM room WHERE room_user_id =  ".$_SESSION['login_user_id'].")";
+                            $qry = $conn->query("SELECT * FROM students $where order by stud_room_id asc");
+                            while($row = $qry->fetch_assoc()):?>
+                            <tr>
+                                <td><?php echo $i++ ?></td>
+                                <td><a><?php echo ucwords($row['stud_room_id']) ?></a></td>
+                                <td class="project_progress">
+                                    <a>
+                                        <?php
+                                            $qry2 = $conn->query("SELECT user_name, user_surname FROM users WHERE user_id = ".$row['stud_user_id']);
+                                            $row2 = $qry2->fetch_assoc();
+                                            echo $row2['user_name'];
+                                        ?>
+                                    </a>
+                                </td>
+                                <td class="project_progress">
+                                    <a><?php echo $row2['user_surname'] ?></a>
+                                </td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-12">
+                <div class="small-box bg-light shadow-sm border">
+                    <div class="inner">
+                        <h3><?php echo $conn->query("SELECT * FROM students $where")->num_rows; ?></h3>
+                        <p>Total Students</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-layer-group"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif;?>
+
+
+<?php if($_SESSION['login_user_type_id'] == 4): ?>
+<div class="row">
+    <div class="col-8">
+        <div class="card card-outline card-success">
+            <div class="card-header">
+                <b>Students In Room</b>
+            </div>
+            <div class="card-body p-0">
+                <div class="table-responsive">
+                    <table class="table m-0 table-hover">
+                        <colgroup>
+                            <col width="10%">
+                            <col width="30%">
+                            <col width="30%">
+                            <col width="30%">
+                        </colgroup>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Mess ID</th>
+                                <th>Student Name</th>
+                                <th>Student Surname</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $i = 1;
+
+                            $where = "WHERE stud_mess_id IN ( SELECT mess_id  FROM mess WHERE mess_user_id =  ".$_SESSION['login_user_id'].")";
+                            $qry = $conn->query("SELECT * FROM students $where order by stud_mess_id asc");
+                            while($row = $qry->fetch_assoc()):?>
+                            <tr>
+                                <td><?php echo $i++ ?></td>
+                                <td><a><?php echo ucwords($row['stud_mess_id']) ?></a></td>
+                                <td class="project_progress">
+                                    <a>
+                                        <?php
+                                            $qry2 = $conn->query("SELECT user_name, user_surname FROM users WHERE user_id = ".$row['stud_user_id']);
+                                            $row2 = $qry2->fetch_assoc();
+                                            echo $row2['user_name'];
+                                        ?>
+                                    </a>
+                                </td>
+                                <td class="project_progress">
+                                    <a><?php echo $row2['user_surname'] ?></a>
+                                </td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-4">
+        <div class="row">
+            <div class="col-12 col-sm-6 col-md-12">
+                <div class="small-box bg-light shadow-sm border">
+                    <div class="inner">
+                        <h3><?php echo $conn->query("SELECT * FROM students $where")->num_rows; ?></h3>
+                        <p>Total Students</p>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-layer-group"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php endif;?>
